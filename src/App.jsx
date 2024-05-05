83623b31393d4c31263712b7f504df31883d0bc1
Gustavo França <gstvz.franca@gmail.com>
2024-05-05 17:50:32 -0300

    feat: created Home page and set it up temporarily on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import './App.css'
+import { Home } from './views/pages/home'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Home />
   )
 }
 
---
+++ src/views/pages/home.jsx
@@ -0,0 +1,7 @@
+import { Layout } from "../components/layout/layout";
+
+export function Home() {
+  return (
+    <Layout />
+  )
+}
