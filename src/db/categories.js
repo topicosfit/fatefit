7b4aa86995670d54fb1276b50b3ea716471177cd
Gustavo França <gstvz.franca@gmail.com>
2024-05-18 13:43:06 -0300

    feat: created products and categories lists

---
+++ src/db/categories.js
@@ -0,0 +1,22 @@
+export const categories = [
+    {
+        "id_category": 1,
+        "st_name": "ERGOMÉTRICOS", 
+        "url": ""
+    },
+    {
+        "id_category": 2,
+        "st_name": "EQUIPAMENTOS", 
+        "url": ""
+    },
+    {
+        "id_category": 3,
+        "st_name": "ACESSÓRIOS", 
+        "url": ""
+    },
+    {
+        "id_category": 4,
+        "st_name": "HOMEBOX", 
+        "url": ""
+    }
+]
---
+++ src/db/products.js
@@ -0,0 +1,47 @@
+import anilha from "../assets/products/anilha.webp";
+
+export const products = [
+    {
+        "id_product": 1,
+        "st_name": "Halter 5kg",
+        "st_image_url": anilha,
+        "st_description": "",
+        "nm_price": 50,
+    },
+    {
+        "id_product": 1,
+        "st_name": "Barra Olímpica",
+        "st_description": "",
+        "nm_price": 5075,
+    },
+    {
+        "id_product": 1,
+        "st_name": "Teu pai",
+        "st_description": "Luva de treino",
+        "nm_price": 50000,
+    },
+    {
+        "id_product": 1,
+        "st_name": "",
+        "st_description": "Corda",
+        "nm_price": 0,
+    },
+    {
+        "id_product": 1,
+        "st_name": "",
+        "st_description": "Corda",
+        "nm_price": 0,
+    },
+    {
+        "id_product": 1,
+        "st_name": "",
+        "st_description": "Corda",
+        "nm_price": 0,
+    },
+    {
+        "id_product": 1,
+        "st_name": "",
+        "st_description": "Corda",
+        "nm_price": 0,
+    }
+]
