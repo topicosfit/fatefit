07e21e9f1a41c4af77db8ccb1b006403c0f8158b
Gustavo França <gstvz.franca@gmail.com>
2024-06-04 20:20:57 -0300

    feat: implemented form and react-hook-form on Checkout-form

--- src/views/components/checkout/checkout-form/checkout-form.jsx
+++ src/views/components/checkout/checkout-form/checkout-form.jsx
@@ -1,32 +1,61 @@
+import { useForm } from "react-hook-form";
 import { Button } from "../../button/button";
 import { AddressInfo } from "../address-info/address-info";
 import { PaymentInfo } from "../payment-info/payment-info";
 import { PersonalInfo } from "../personal-info/personal-info";
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../../../../app/context/cart/cart-context-hook";
 
 export function CheckoutForm() {
-    return (
-      <div className="
+  const navigate = useNavigate();
+  const { clearCart } = useCart();
+
+  const {
+    handleSubmit, 
+    register, 
+    reset, 
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const handleOnSubmit = async (data) => {
+    const { name, cpf, email, phone, address, number, neighborhood, city, state} = data;
+
+    clearCart();
+    reset();
+    navigate('/');
+  };
+
+  return (
+    <form className="
+        flex
+        flex-col
+        gap-8
+    "
+      onSubmit={handleSubmit(handleOnSubmit)}
+    >
+        <div className="
+          flex
+          gap-8
+        ">
+          <PersonalInfo register={register} />
+          <AddressInfo register={register} />
+        </div>
+        <div className="
           flex
           flex-col
           gap-8
-      ">
-          <div className="
-            flex
-            gap-8
-          ">
-            <PersonalInfo />
-            <AddressInfo />
+        ">
+          <PaymentInfo />
+          <div className="flex justify-center">
+            <Button 
+              background="#42AD60" 
+              text="FINALIZAR" 
+              textColor="white" 
+              type="submit" 
+            />
           </div>
-          <div className="
-            flex
-            flex-col
-            gap-8
-          ">
-            <PaymentInfo />
-            <div className="flex justify-center">
-              <Button background="#42AD60" text="FINALIZAR" textColor="white" />
-            </div>
-          </div>          
-      </div>
-    )
+        </div>          
+    </form>
+  )
 }
